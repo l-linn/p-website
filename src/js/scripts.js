2e400b4808ee8cd4a0d7446014da3b29f9029134
lin <shilinxi@proton.me>
2023-10-01 21:56:17 +0100

    Create scripts.js

---
+++ src/js/scripts.js
@@ -0,0 +1,16 @@
+//Document Object Model (DOM) is a programming interface for HTML. 
+//two built-in functions to select nodes in the DOM
+//both functions take a CSS selector string as a parameter
+document.querySelector('a'); //use document as the node
+document.querySelectorAll('a');
+//examples - using 'a' element in html/css
+let firstA = document.querySelector('a');
+console.log(firstA);
+console.log(firstA.length);//undefined - only one element
+console.log(firstA[0]);//undefined
+
+let a = document.querySelectorAll('a');
+console.log(a);
+console.log(a.length);
+console.log(a[0]);
+console.log(a[7]);
